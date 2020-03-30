14adccf80117c4261cb62b0b8d9e0372cb8a32dd
Robin737 <56026818+Robin737@users.noreply.github.com>
2020-03-30 09:09:43 +0200

    Add files via upload

---
+++ Template_system/index.php
@@ -0,0 +1,14 @@
+
+<?php
+require "Template.php";
+
+$data = array(
+    "title" => "popisek stránky",
+    "message" => "zpráva",
+    "h1" => "nadpis H1"
+);
+
+$test = new Template();
+$test->setData($data);
+$test->setFileName("samohyl.html");
+$test->render();
